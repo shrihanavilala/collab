56985cb6a8c53164d6dd7ebf6edfc04d0c07c2a8
Gator Donald <g4t0rd4r4pp3r@gmail.com>
2023-11-20 14:00:08 -0600

    added elif

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ pw = input("Is your passord strong? ")
 auth = input("Do you use multi-factor authentication? ")
 enc = input("Do you know how to encrypt sensitive information? ")
 
-if (phish =='y' and phish =='n' and pw =='y' and pw =='n' and auth =='y' and auth =='n' and enc =='y' and enc =='n'):
+if (phish =='y' and pw =='y' and auth =='y' and enc =='y'):
     print("You have good security habits.")
-else:
+elif (phish == 'n' and pw == 'n' and auth == 'n' and enc == 'n'):
   print("You can improve your security habits.")
 
